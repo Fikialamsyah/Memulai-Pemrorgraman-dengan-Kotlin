72416405f8e5d0935f01374e4c61391d05de76d9
Fiki Alamsyah <fikialamsyah16@gmai.com>
2020-10-14 09:21:42 +0700

    Menambahkan folder 7- Numbers

---
+++ 7 - Numbers/src/com/dicoding/kotlinfundamental/numbers/Numbers.kt
@@ -0,0 +1,15 @@
+package com.dicoding.kotlinfundamental.numbers
+
+fun main() {
+    val maxInt = Int.MAX_VALUE
+    val minInt = Int.MIN_VALUE
+
+    println(maxInt)
+    println(minInt)
+
+    val numberOne = 1
+    val numberTwo = 2
+    println(numberOne + numberTwo)
+
+
+}
