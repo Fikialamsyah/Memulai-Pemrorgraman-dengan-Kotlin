b0cb7854a728b3ebd7a3a20322a38db569da2212
Fiki Alamsyah <fikialamsyah16@gmai.com>
2020-10-14 09:18:11 +0700

    menambahkan folder 5 - Arrays

---
+++ 4- Arrays/src/com/dicoding/kotlinfundamental/arrays/Arrays.kt
@@ -0,0 +1,13 @@
+package com.dicoding.kotlinfundamental.arrays
+
+fun main() {
+    // mix array
+    val mixArray = arrayOf(1, 3, 5, 7,"Dicoding", true)
+    println(mixArray)
+
+    // int array
+    val intArray = intArrayOf(1, 3, 5, 7)
+    println(intArray)
+
+
+}
